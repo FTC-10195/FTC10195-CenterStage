a37e77b15010a0a9b1efafe788bac3d33c4b9a02
ayaamghi <ayaamghi@gmail.com>
2023-09-15 19:38:27 -0400

    Create thing.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/thing.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+@TeleOp
+public class thing extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotorEx main = (DcMotorEx) hardwareMap.get("test");
+        boolean previousA = gamepad1.a;
+        waitForStart();
+        if (isStopRequested()) return;
+        while (opModeIsActive()) {
+            while (gamepad1.a && !previousA) {
+                main.setPower(1);
+                sleep(2000)
+
+            }
+            previousA = gamepad1.a;
+
+        }
+
+    }
+}
